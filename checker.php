8af7d6cbb1792ce1acab4d4e08b5d5940c55b1cb
PhenixTN <43891661+PhenixTN@users.noreply.github.com>
2018-10-27 19:04:14 +0100

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,9 +1,18 @@
 <?php
 /*
-	Issued Feb 9th, 2018
+    Coded By PhenixTN
+	Thanks to : Mr.Anderson
+	mr.phenix2012@gmail.com
+	Issued Oct 27th, 2018
 */
+/*
 
-$keyword = array("Webshell", "0byt3m1n1", "IndoXploit","Shell","shell","wso"," Upload Please ","ImamBro"); // Edit Here
+Lamerz Don't change my fucking code :D
+
+Cuz u will still a n00b ^o^
+
+*/
+$keyword = array("Webshell", "0byt3m1n1", "IndoXploit","Shell","shell","wso"," Upload Please ","Upload Please"); // Edit Here
 
 if(!empty($_POST['check'])) {
 	$list = $_POST['list'];
@@ -33,13 +42,13 @@ fclose($file);
 function eof() {$off = chr(90).chr(101).chr(114).chr(111).chr(66).chr(121).chr(116).chr(101);eval(chr(101).chr(99).chr(104).chr(111)." $off.'.".chr(73).chr(68)."';");}
 echo "<html>\n";
 echo "<head>\n";
-echo "<title>Webshell Mass Checker By Phenix-TN</title>";
-echo '<style type="text/css">body{color="blue";background:#111111;font-family:\'Courier\'}textarea{color="blue";background:transparent;border-color="blue";padding:5px;}input{color:#111111;background:#009900;margin-top:10px;font-size:20px;border:2px double #009900;padding:2px;padding-left:150px;padding-right:150px;font-family:Arial}</style>';
+echo "<title>Webshell Mass Checker By PhenixTN</title>";
+echo '<style type="text/css">body{color:#009900;background:#111111;font-family:\'Courier\'}textarea{color:#009900;background:transparent;border-color:#009900;padding:5px;}input{color:#111111;background:#009900;margin-top:10px;font-size:20px;border:2px double #009900;padding:2px;padding-left:150px;padding-right:150px;font-family:Arial}</style>';
 echo "\n</head>";
 echo "<body>\n";
 echo "<center>";
 if(empty($_POST['check'])) {
-	echo "<h1>Webshell Mass Checker</h1>\n";
+	echo "<h1>Webshell Mass Checker By Phenix-TN</h1>\n";
 	echo '<form method="post">'."\n";
 	echo '<textarea name="list" placeholder="https://site.com/shell.php" style="width:700px;height:250px;">'."\n";
 	echo "</textarea><br>\n";
@@ -50,5 +59,5 @@ echo "</center>\n";
 echo "</body>\n";
 echo "</html>";
 
-// Recoded? only changed and delete copyright? Don't be a bastard dude!
+
 ?>
